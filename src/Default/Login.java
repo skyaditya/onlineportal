98baa4d27a633709ea5333448758335265fe78fa
sky347 <44710281+sky347@users.noreply.github.com>
2018-11-12 19:52:44 +0530

    wtf

--- src/Default/Login.java
+++ src/Default/Login.java
@@ -9,6 +9,7 @@ import login.Login_Details;
 public class Login extends JFrame {
 	
 	public static void main(String[] args) {
+		System.out.print("wtf");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
